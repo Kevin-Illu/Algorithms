8b0deeb76fae4de538fdaa3b948f02ea00b44c00
kevin-Illu <78170908+Kevin-Illu@users.noreply.github.com>
2023-09-05 19:19:03 -0600

    xD

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Algorithms_1 = require("./Algorithms");
+console.log((0, Algorithms_1.binarySearch)([1, 32, 41, 51, 66], 32));
